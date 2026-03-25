e619eecddbfd4d8a958d550266159c50d3ff790a
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2026-03-25 01:42:39 -0500

    added basic debug flag handling

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main() {
 	var events chan Event = make(chan Event)
+
+	debugMode := flag.Bool("debug", false, "to enable keyboard debugging information")
+	flag.Parse()
+
 	go terminalListener(events)
 	go resizeListener(events)
 
+	if *debugMode {
+		KeyboardDebugging(events)
+	}
+}
+
+func KeyboardDebugging(events <-chan Event) {
 	for event := range events {
-		fmt.Println(event)
 		if event.Type == ENUM_EVENT_KEY {
 			if event.KeyData.Key == "CTRL+C" {
 				return
